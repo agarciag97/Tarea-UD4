ea481cc9694469b22c6d80cf799503b9b54a2728
agarciag97 <agarciag@campusdigitalfp.com>
2025-05-14 10:49:21 +0200

    Network Manager

---
+++ Alex-UD4/Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    Application.targetFrameRate = 60;
+    SceneManager.LoadScene(1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
