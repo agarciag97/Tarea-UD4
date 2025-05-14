44496e93020877e539f18b1a323af8f77d478548
agarciag97 <agarciag@campusdigitalfp.com>
2025-05-14 12:09:46 +0200

    IU Manager

---
+++ Alex-UD4/Assets/Scripts/IU_Manager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+
+public class IU_Manager : MonoBehaviour
+{
+    [Header(" Panels ")]
+    [SerializeField] private GameObject connectionPannel;
+    [SerializeField] private GameObject waitingPannel;
+    [SerializeField] private GameObject gamePannel;
+
+    void Start()
+    {
+        ShowConnectionPannel();
+    }
+
+    // Update is called once per frame
+    void Update(){
+    }
+        private void ShowConnectionPannel() {
+        connectionPannel.SetActive(true);
+        waitingPannel.SetActive(false);
+        gamePannel.SetActive(false);
+        }
+        private void ShowWaitingPannel() {
+        connectionPannel.SetActive(false);
+        waitingPannel.SetActive(true);
+        gamePannel.SetActive(false);
+        }
+        private void ShowGamePannel() {
+        connectionPannel.SetActive(false);
+        waitingPannel.SetActive(false);
+        gamePannel.SetActive(true);
+        }
+        public void HostButtonCallback() {
+        NetworkManager.Singleton.StartHost();
+            ShowWaitingPannel();
+        }
+        public void ClientButtonCallback() {
+        NetworkManager.Singleton.StartClient();
+            ShowWaitingPannel();
+        }
+
+    }
+
